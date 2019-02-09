f3988e627bc34e2a0e90f1a176d2717b9f5099bc
videosambo <onnikontikorpi@gmail.com>
2019-02-09 21:35:41 +0200

    Added resources

--- 2DMultiplayerShooter/src/com/superogi/client/renderEngine/graphics/ImageLoader.java
+++ 2DMultiplayerShooter/src/com/superogi/client/renderEngine/graphics/ImageLoader.java
@@ -1,6 +1,7 @@
 package com.superogi.client.renderEngine.graphics;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -8,6 +9,13 @@ import javax.imageio.ImageIO;
 public class ImageLoader {
 	
 	public static BufferedImage loadImage(String path) {
+		
+		File f = new File(path);
+		if (!f.exists()) {
+			System.err.println("Cannot found image at " + path);
+			System.exit(1);
+		}
+		
 		try {
 			return ImageIO.read(ImageLoader.class.getResource(path));
 		} catch (IOException e) {
