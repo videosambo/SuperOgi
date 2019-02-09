5023a706b194ac464633c1e43b52fd98bcadc655
videosambo <onnikontikorpi@gmail.com>
2019-02-09 20:12:30 +0200

    Display class finished

--- 2DMultiplayerShooter/src/com/superogi/client/renderengine/Display.java
+++ 2DMultiplayerShooter/src/com/superogi/client/renderengine/Display.java
@@ -1,6 +1,7 @@
 package com.superogi.client.renderengine;
 
 import java.awt.Canvas;
+import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
@@ -23,6 +24,22 @@ public class Display {
 
 	private void createDisplay() {
 		
+		frame = new JFrame(title);
+		frame.setSize(width, height);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+		Dimension dimension = new Dimension(width, height);
+		
+		canvas = new Canvas();
+		canvas.setPreferredSize(dimension);
+		canvas.setMinimumSize(dimension);
+		canvas.setMaximumSize(dimension);
+		
+		frame.add(canvas);
+		frame.pack();
 		
 	}
 
